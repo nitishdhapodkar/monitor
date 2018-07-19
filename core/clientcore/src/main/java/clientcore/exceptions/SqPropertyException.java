06df42181a00dd5cb165f5e373454cb88c5db026
Nitish <nitish.dhapodkar@sequretek.com>
2018-07-19 09:19:53 +0530

    Added two projects in for common stuffs and logger

---
+++ core/clientcore/src/main/java/clientcore/exceptions/SqPropertyException.java
@@ -0,0 +1,30 @@
+package clientcore.exceptions;
+
+public class SqPropertyException extends Exception{
+	
+	private final ErrorCode code;
+
+	public SqPropertyException(ErrorCode code) {
+		super();
+		this.code = code;
+	}
+
+	public SqPropertyException(String message, Throwable cause, ErrorCode code) {
+		super(message, cause);
+		this.code = code;
+	}
+
+	public SqPropertyException(String message, ErrorCode code) {
+		super(message);
+		this.code = code;
+	}
+
+	public SqPropertyException(Throwable cause, ErrorCode code) {
+		super(cause);
+		this.code = code;
+	}
+	
+	public ErrorCode getCode() {
+		return this.code;
+	}
+}
---
+++ core/clientcore/src/main/java/core/util/files/PropertyLoader.java
@@ -0,0 +1,19 @@
+package core.util.files;
+
+import java.util.Map;
+
+public class PropertyLoader {
+	
+	public Map<String, String> loadProperties(String filePath) {
+		Map<String, String> properties = null;
+		
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return properties;
+	}
+
+}
